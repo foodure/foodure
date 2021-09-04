e85bd47c2e4101b38da89034efd2b568868c8417
saedjabali <saad.jabali@yahoo.com>
2021-09-04 21:40:04 +0300

    adding auth

--- app/src/main/java/com/foodure/view/ProfileActivity.java
+++ app/src/main/java/com/foodure/view/ProfileActivity.java
@@ -13,42 +13,10 @@ import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_profile);
-    Objects.requireNonNull(getSupportActionBar()).hide();
-
-    TextView usernameBtn = findViewById(R.id.username_profile);
-    ImageView usernameImg = findViewById(R.id.profile_profile);
-
-    TextView settingsBtn = findViewById(R.id.settings_profile);
-    ImageView settingsImg = findViewById(R.id.settingsImg_profile);
-
-    usernameBtn.setOnClickListener(v -> {
-      goToProfile();
-    });
-
-    usernameImg.setOnClickListener(v -> {
-      goToProfile();
-    });
-
-    settingsBtn.setOnClickListener(v -> {
-      goToSettings();
-    });
-
-    settingsImg.setOnClickListener(v -> {
-      goToSettings();
-    });
-  }
-
-  public void goToProfile(){
-    Intent goToProfile = new Intent(ProfileActivity.this, ProfileActivity.class);
-    startActivity(goToProfile);
-  }
-
-  public void goToSettings(){
-    Intent goToSettings = new Intent(ProfileActivity.this, SettingsActivity.class);
-    startActivity(goToSettings);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+    }
 }
