ae0df718a46826ef168de1c7a480bb46a0bbb96f
MKAbuMattar <mohammad.khaled@outlook.com>
2021-09-05 11:35:50 +0300

    graphql

---
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/Cart.java
@@ -0,0 +1,212 @@
+package com.amplifyframework.datastore.generated.model;
+
+import com.amplifyframework.core.model.annotations.HasMany;
+import com.amplifyframework.core.model.temporal.Temporal;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.Objects;
+
+import androidx.core.util.ObjectsCompat;
+
+import com.amplifyframework.core.model.Model;
+import com.amplifyframework.core.model.annotations.Index;
+import com.amplifyframework.core.model.annotations.ModelConfig;
+import com.amplifyframework.core.model.annotations.ModelField;
+import com.amplifyframework.core.model.query.predicate.QueryField;
+
+import static com.amplifyframework.core.model.query.predicate.QueryField.field;
+
+/** This is an auto generated class representing the Cart type in your schema. */
+@SuppressWarnings("all")
+@ModelConfig(pluralName = "Carts")
+public final class Cart implements Model {
+  public static final QueryField ID = field("Cart", "id");
+  public static final QueryField USERNAME = field("Cart", "username");
+  public static final QueryField LOCATION = field("Cart", "location");
+  private final @ModelField(targetType="ID", isRequired = true) String id;
+  private final @ModelField(targetType="String", isRequired = true) String username;
+  private final @ModelField(targetType="String", isRequired = true) String location;
+  private final @ModelField(targetType="FoodPost") @HasMany(associatedWith = "cart", type = FoodPost.class) List<FoodPost> cartPosts = null;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime createdAt;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime updatedAt;
+  public String getId() {
+      return id;
+  }
+  
+  public String getUsername() {
+      return username;
+  }
+  
+  public String getLocation() {
+      return location;
+  }
+  
+  public List<FoodPost> getCartPosts() {
+      return cartPosts;
+  }
+  
+  public Temporal.DateTime getCreatedAt() {
+      return createdAt;
+  }
+  
+  public Temporal.DateTime getUpdatedAt() {
+      return updatedAt;
+  }
+  
+  private Cart(String id, String username, String location) {
+    this.id = id;
+    this.username = username;
+    this.location = location;
+  }
+  
+  @Override
+   public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      } else if(obj == null || getClass() != obj.getClass()) {
+        return false;
+      } else {
+      Cart cart = (Cart) obj;
+      return ObjectsCompat.equals(getId(), cart.getId()) &&
+              ObjectsCompat.equals(getUsername(), cart.getUsername()) &&
+              ObjectsCompat.equals(getLocation(), cart.getLocation()) &&
+              ObjectsCompat.equals(getCreatedAt(), cart.getCreatedAt()) &&
+              ObjectsCompat.equals(getUpdatedAt(), cart.getUpdatedAt());
+      }
+  }
+  
+  @Override
+   public int hashCode() {
+    return new StringBuilder()
+      .append(getId())
+      .append(getUsername())
+      .append(getLocation())
+      .append(getCreatedAt())
+      .append(getUpdatedAt())
+      .toString()
+      .hashCode();
+  }
+  
+  @Override
+   public String toString() {
+    return new StringBuilder()
+      .append("Cart {")
+      .append("id=" + String.valueOf(getId()) + ", ")
+      .append("username=" + String.valueOf(getUsername()) + ", ")
+      .append("location=" + String.valueOf(getLocation()) + ", ")
+      .append("createdAt=" + String.valueOf(getCreatedAt()) + ", ")
+      .append("updatedAt=" + String.valueOf(getUpdatedAt()))
+      .append("}")
+      .toString();
+  }
+  
+  public static UsernameStep builder() {
+      return new Builder();
+  }
+  
+  /** 
+   * WARNING: This method should not be used to build an instance of this object for a CREATE mutation.
+   * This is a convenience method to return an instance of the object with only its ID populated
+   * to be used in the context of a parameter in a delete mutation or referencing a foreign key
+   * in a relationship.
+   * @param id the id of the existing item this instance will represent
+   * @return an instance of this model with only ID populated
+   * @throws IllegalArgumentException Checks that ID is in the proper format
+   */
+  public static Cart justId(String id) {
+    try {
+      UUID.fromString(id); // Check that ID is in the UUID format - if not an exception is thrown
+    } catch (Exception exception) {
+      throw new IllegalArgumentException(
+              "Model IDs must be unique in the format of UUID. This method is for creating instances " +
+              "of an existing object with only its ID field for sending as a mutation parameter. When " +
+              "creating a new object, use the standard builder method and leave the ID field blank."
+      );
+    }
+    return new Cart(
+      id,
+      null,
+      null
+    );
+  }
+  
+  public CopyOfBuilder copyOfBuilder() {
+    return new CopyOfBuilder(id,
+      username,
+      location);
+  }
+  public interface UsernameStep {
+    LocationStep username(String username);
+  }
+  
+
+  public interface LocationStep {
+    BuildStep location(String location);
+  }
+  
+
+  public interface BuildStep {
+    Cart build();
+    BuildStep id(String id) throws IllegalArgumentException;
+  }
+  
+
+  public static class Builder implements UsernameStep, LocationStep, BuildStep {
+    private String id;
+    private String username;
+    private String location;
+    @Override
+     public Cart build() {
+        String id = this.id != null ? this.id : UUID.randomUUID().toString();
+        
+        return new Cart(
+          id,
+          username,
+          location);
+    }
+    
+    @Override
+     public LocationStep username(String username) {
+        Objects.requireNonNull(username);
+        this.username = username;
+        return this;
+    }
+    
+    @Override
+     public BuildStep location(String location) {
+        Objects.requireNonNull(location);
+        this.location = location;
+        return this;
+    }
+    
+    /** 
+     * @param id id
+     * @return Current Builder instance, for fluent method chaining
+     */
+    public BuildStep id(String id) {
+        this.id = id;
+        return this;
+    }
+  }
+  
+
+  public final class CopyOfBuilder extends Builder {
+    private CopyOfBuilder(String id, String username, String location) {
+      super.id(id);
+      super.username(username)
+        .location(location);
+    }
+    
+    @Override
+     public CopyOfBuilder username(String username) {
+      return (CopyOfBuilder) super.username(username);
+    }
+    
+    @Override
+     public CopyOfBuilder location(String location) {
+      return (CopyOfBuilder) super.location(location);
+    }
+  }
+  
+}
---
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/Restaurant.java
@@ -0,0 +1,276 @@
+package com.amplifyframework.datastore.generated.model;
+
+import com.amplifyframework.core.model.annotations.HasMany;
+import com.amplifyframework.core.model.temporal.Temporal;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.Objects;
+
+import androidx.core.util.ObjectsCompat;
+
+import com.amplifyframework.core.model.Model;
+import com.amplifyframework.core.model.annotations.Index;
+import com.amplifyframework.core.model.annotations.ModelConfig;
+import com.amplifyframework.core.model.annotations.ModelField;
+import com.amplifyframework.core.model.query.predicate.QueryField;
+
+import static com.amplifyframework.core.model.query.predicate.QueryField.field;
+
+/** This is an auto generated class representing the Restaurant type in your schema. */
+@SuppressWarnings("all")
+@ModelConfig(pluralName = "Restaurants")
+public final class Restaurant implements Model {
+  public static final QueryField ID = field("Restaurant", "id");
+  public static final QueryField EMAIL = field("Restaurant", "email");
+  public static final QueryField TITLE = field("Restaurant", "title");
+  public static final QueryField USERNAME = field("Restaurant", "username");
+  public static final QueryField LOCATION = field("Restaurant", "location");
+  private final @ModelField(targetType="ID", isRequired = true) String id;
+  private final @ModelField(targetType="String", isRequired = true) String email;
+  private final @ModelField(targetType="String", isRequired = true) String title;
+  private final @ModelField(targetType="String", isRequired = true) String username;
+  private final @ModelField(targetType="String", isRequired = true) String location;
+  private final @ModelField(targetType="FoodPost") @HasMany(associatedWith = "restaurant", type = FoodPost.class) List<FoodPost> restaurantPost = null;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime createdAt;
+  private @ModelField(targetType="AWSDateTime", isReadOnly = true) Temporal.DateTime updatedAt;
+  public String getId() {
+      return id;
+  }
+  
+  public String getEmail() {
+      return email;
+  }
+  
+  public String getTitle() {
+      return title;
+  }
+  
+  public String getUsername() {
+      return username;
+  }
+  
+  public String getLocation() {
+      return location;
+  }
+  
+  public List<FoodPost> getRestaurantPost() {
+      return restaurantPost;
+  }
+  
+  public Temporal.DateTime getCreatedAt() {
+      return createdAt;
+  }
+  
+  public Temporal.DateTime getUpdatedAt() {
+      return updatedAt;
+  }
+  
+  private Restaurant(String id, String email, String title, String username, String location) {
+    this.id = id;
+    this.email = email;
+    this.title = title;
+    this.username = username;
+    this.location = location;
+  }
+  
+  @Override
+   public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      } else if(obj == null || getClass() != obj.getClass()) {
+        return false;
+      } else {
+      Restaurant restaurant = (Restaurant) obj;
+      return ObjectsCompat.equals(getId(), restaurant.getId()) &&
+              ObjectsCompat.equals(getEmail(), restaurant.getEmail()) &&
+              ObjectsCompat.equals(getTitle(), restaurant.getTitle()) &&
+              ObjectsCompat.equals(getUsername(), restaurant.getUsername()) &&
+              ObjectsCompat.equals(getLocation(), restaurant.getLocation()) &&
+              ObjectsCompat.equals(getCreatedAt(), restaurant.getCreatedAt()) &&
+              ObjectsCompat.equals(getUpdatedAt(), restaurant.getUpdatedAt());
+      }
+  }
+  
+  @Override
+   public int hashCode() {
+    return new StringBuilder()
+      .append(getId())
+      .append(getEmail())
+      .append(getTitle())
+      .append(getUsername())
+      .append(getLocation())
+      .append(getCreatedAt())
+      .append(getUpdatedAt())
+      .toString()
+      .hashCode();
+  }
+  
+  @Override
+   public String toString() {
+    return new StringBuilder()
+      .append("Restaurant {")
+      .append("id=" + String.valueOf(getId()) + ", ")
+      .append("email=" + String.valueOf(getEmail()) + ", ")
+      .append("title=" + String.valueOf(getTitle()) + ", ")
+      .append("username=" + String.valueOf(getUsername()) + ", ")
+      .append("location=" + String.valueOf(getLocation()) + ", ")
+      .append("createdAt=" + String.valueOf(getCreatedAt()) + ", ")
+      .append("updatedAt=" + String.valueOf(getUpdatedAt()))
+      .append("}")
+      .toString();
+  }
+  
+  public static EmailStep builder() {
+      return new Builder();
+  }
+  
+  /** 
+   * WARNING: This method should not be used to build an instance of this object for a CREATE mutation.
+   * This is a convenience method to return an instance of the object with only its ID populated
+   * to be used in the context of a parameter in a delete mutation or referencing a foreign key
+   * in a relationship.
+   * @param id the id of the existing item this instance will represent
+   * @return an instance of this model with only ID populated
+   * @throws IllegalArgumentException Checks that ID is in the proper format
+   */
+  public static Restaurant justId(String id) {
+    try {
+      UUID.fromString(id); // Check that ID is in the UUID format - if not an exception is thrown
+    } catch (Exception exception) {
+      throw new IllegalArgumentException(
+              "Model IDs must be unique in the format of UUID. This method is for creating instances " +
+              "of an existing object with only its ID field for sending as a mutation parameter. When " +
+              "creating a new object, use the standard builder method and leave the ID field blank."
+      );
+    }
+    return new Restaurant(
+      id,
+      null,
+      null,
+      null,
+      null
+    );
+  }
+  
+  public CopyOfBuilder copyOfBuilder() {
+    return new CopyOfBuilder(id,
+      email,
+      title,
+      username,
+      location);
+  }
+  public interface EmailStep {
+    TitleStep email(String email);
+  }
+  
+
+  public interface TitleStep {
+    UsernameStep title(String title);
+  }
+  
+
+  public interface UsernameStep {
+    LocationStep username(String username);
+  }
+  
+
+  public interface LocationStep {
+    BuildStep location(String location);
+  }
+  
+
+  public interface BuildStep {
+    Restaurant build();
+    BuildStep id(String id) throws IllegalArgumentException;
+  }
+  
+
+  public static class Builder implements EmailStep, TitleStep, UsernameStep, LocationStep, BuildStep {
+    private String id;
+    private String email;
+    private String title;
+    private String username;
+    private String location;
+    @Override
+     public Restaurant build() {
+        String id = this.id != null ? this.id : UUID.randomUUID().toString();
+        
+        return new Restaurant(
+          id,
+          email,
+          title,
+          username,
+          location);
+    }
+    
+    @Override
+     public TitleStep email(String email) {
+        Objects.requireNonNull(email);
+        this.email = email;
+        return this;
+    }
+    
+    @Override
+     public UsernameStep title(String title) {
+        Objects.requireNonNull(title);
+        this.title = title;
+        return this;
+    }
+    
+    @Override
+     public LocationStep username(String username) {
+        Objects.requireNonNull(username);
+        this.username = username;
+        return this;
+    }
+    
+    @Override
+     public BuildStep location(String location) {
+        Objects.requireNonNull(location);
+        this.location = location;
+        return this;
+    }
+    
+    /** 
+     * @param id id
+     * @return Current Builder instance, for fluent method chaining
+     */
+    public BuildStep id(String id) {
+        this.id = id;
+        return this;
+    }
+  }
+  
+
+  public final class CopyOfBuilder extends Builder {
+    private CopyOfBuilder(String id, String email, String title, String username, String location) {
+      super.id(id);
+      super.email(email)
+        .title(title)
+        .username(username)
+        .location(location);
+    }
+    
+    @Override
+     public CopyOfBuilder email(String email) {
+      return (CopyOfBuilder) super.email(email);
+    }
+    
+    @Override
+     public CopyOfBuilder title(String title) {
+      return (CopyOfBuilder) super.title(title);
+    }
+    
+    @Override
+     public CopyOfBuilder username(String username) {
+      return (CopyOfBuilder) super.username(username);
+    }
+    
+    @Override
+     public CopyOfBuilder location(String location) {
+      return (CopyOfBuilder) super.location(location);
+    }
+  }
+  
+}
