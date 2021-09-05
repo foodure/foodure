f78c68383c9d4c06048477cb911ffa6909ab3bf9
saedjabali <saad.jabali@yahoo.com>
2021-09-05 11:11:09 +0300

    models

--- app/src/main/java/com/foodure/view/SignupActivity.java
+++ app/src/main/java/com/foodure/view/SignupActivity.java
@@ -18,53 +18,53 @@ import java.util.Objects;
 
 public class SignupActivity extends AppCompatActivity {
 
-  private static final String TAG = "SignupActivity";
+    private static final String TAG = "SignupActivity";
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_signup);
-    Objects.requireNonNull(getSupportActionBar()).hide();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
-    EditText emailInput = findViewById(R.id.editTextEmail_signup);
-    EditText usernameInput = findViewById(R.id.editTextUsername_signup);
-    EditText passwordInput = findViewById(R.id.editTextPassword_signup);
-    Button signupBtn = findViewById(R.id.signupBtn);
-    Button LoginBtn = findViewById(R.id.gotToLoginBtn);
+        EditText emailInput = findViewById(R.id.editTextEmail_signup);
+        EditText usernameInput = findViewById(R.id.editTextUsername_signup);
+        EditText passwordInput = findViewById(R.id.editTextPassword_signup);
+        Button signupBtn = findViewById(R.id.signupBtn);
+        Button LoginBtn = findViewById(R.id.gotToLoginBtn);
 
-    signupBtn.setOnClickListener(v -> {
-      String email = emailInput.getText().toString();
-      String username = usernameInput.getText().toString();
-      String password = passwordInput.getText().toString();
-      if (!email.isEmpty() && !username.isEmpty() && !password.isEmpty()) {
-        signUp(username, email, password);
-      } else {
-        Toast.makeText(SignupActivity.this, "Please insert your info.??", Toast.LENGTH_SHORT).show();
-      }
-    });
-
-    LoginBtn.setOnClickListener(v -> {
-      Intent goToLogin = new Intent(SignupActivity.this, LoginActivity.class);
-      startActivity(goToLogin);
-    });
-  }
-
-  public void signUp(String username, String email, String password) {
-    Amplify.Auth.signUp(
-        username,
-        password,
-        AuthSignUpOptions.builder()
-            .userAttribute(AuthUserAttributeKey.email(), email)
-            .build(),
-        success -> {
-          Log.i(TAG, "signUp successful: " + success.toString());
-          Intent goToVerification = new Intent(SignupActivity.this, VerificationActivity.class);
-          goToVerification.putExtra("username", username);
-          goToVerification.putExtra("password", password);
-          startActivity(goToVerification);
-        },
-        error -> {
-          Log.e(TAG, "signUp failed: " + error.toString());
+        signupBtn.setOnClickListener(v -> {
+            String email = emailInput.getText().toString();
+            String username = usernameInput.getText().toString();
+            String password = passwordInput.getText().toString();
+            if (!email.isEmpty() && !username.isEmpty() && !password.isEmpty()) {
+                signUp(username, email, password);
+            } else {
+                Toast.makeText(SignupActivity.this, "Please insert your info!", Toast.LENGTH_SHORT).show();
+            }
         });
-  }
+
+        LoginBtn.setOnClickListener(v -> {
+            Intent goToLogin = new Intent(SignupActivity.this, LoginActivity.class);
+            startActivity(goToLogin);
+        });
+    }
+
+    public void signUp(String username, String email, String password) {
+        Amplify.Auth.signUp(
+                username,
+                password,
+                AuthSignUpOptions.builder()
+                        .userAttribute(AuthUserAttributeKey.email(), email)
+                        .build(),
+                success -> {
+                    Log.i(TAG, "signUp successful: " + success.toString());
+                    Intent goToVerification = new Intent(SignupActivity.this, VerificationActivity.class);
+                    goToVerification.putExtra("username", username);
+                    goToVerification.putExtra("password", password);
+                    startActivity(goToVerification);
+                },
+                error -> {
+                    Log.e(TAG, "signUp failed: " + error.toString());
+                });
+    }
 }
